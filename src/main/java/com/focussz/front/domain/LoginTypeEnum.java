238b947996859b703afe70146d52a789a53d611d
Harry <hewewu@cisco.com>
2017-08-06 23:57:13 +0800

    init b2c mvc

---
+++ src/main/java/com/focussz/front/domain/LoginTypeEnum.java
@@ -0,0 +1,32 @@
+package com.focussz.front.domain;
+
+/**
+ * 登陆类型方式
+ */
+public enum LoginTypeEnum {
+	/**
+	 * 本系统账号的用户登陆
+	 */
+	system,
+
+	/**
+	 * qq登陆
+	 */
+	qq,
+
+	/**
+	 * 新浪微博
+	 */
+	sinawb,
+
+	/**
+	 * QQ微博
+	 */
+	qqWeibo,
+
+	/**
+	 * 支付宝快捷
+	 */
+	alipay
+
+}
